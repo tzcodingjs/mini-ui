5d7f82f3736a95480a3686a7999726f0762aee82
tzweb <821189067@qq.com>
2018-12-04 22:35:05 +0800

    picker

--- packages/picker/mini-picker.vue
+++ packages/picker/mini-picker.vue
@@ -3,7 +3,7 @@
     <div class="mini-picker-input" @click.stop="HandleTogglePicker">
       <span>{{ initNum }}</span>
     </div>
-    <div class="mini-picker-bg" v-show="show"></div>
+    <div class="mini-picker-bg" v-show="show" @click.stop="hidePicker"></div>
     <div class="mini-picker-con" v-show="show">
       <!-- 按钮 -->
       <div class="mini-picker-btn">
@@ -12,8 +12,14 @@
       </div>
 
       <!-- picker条目 -->
-      <div class="mini-picker-item">
-        <ul>
+      <div
+        class="mini-picker-item"
+        @touchstart="onTouchStart"
+        @touchmove.prevent="onTouchMove"
+        @touchend="onTouchEnd"
+        @touchcancel="onTouchEnd"
+      >
+        <ul :style="animate">
           <li v-for="(item,index) in num" :key="index">{{ item }}</li>
         </ul>
       </div>
@@ -32,24 +38,64 @@ export default {
     num: {
       type: Array,
       default: () => {
-        return [1,2,3,4,5,6,7,8,9,10];
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
       }
+    },
+    duration: {
+      type: Number,
+      default: 200
+    },
+    itemHeight:{
+      type:Number,
+      default:76
     }
   },
   data() {
     return {
-      show: false
+      show: false,
+      startY: 0,
+      stepY: 0,
+      baseY: 0,
+      selectItem: ""
     };
   },
+  mounted(){
+    this.baseY = this.itemHeight
+  },
+  computed: {
+    animate() {
+      return {
+        transition: `${this.duration}ms`,
+        transform: `translate3d(0, ${this.baseY + this.stepY}px,0)`
+      };
+    }
+  },
   methods: {
+    onTouchStart(e) {
+      this.startY = e.touches[0].clientY;
+    },
+    onTouchMove(e) {
+      this.stepY = e.touches[0].clientY - this.startY;
+    },
+    onTouchEnd(e) {
+      if (this.base == (this.baseY += this.stepY)) {
+        return false
+      }
+      this.stepY = 0;
+    },
     HandleTogglePicker() {
       this.show = true;
     },
+    hidePicker(){
+      this.show = false
+    },
     cancel() {
       this.show = false;
+      this.$emit("cancel");
     },
     confirm() {
       this.show = false;
+      this.$emit("confirm", this.selectItem);
     }
   }
 };
@@ -60,6 +106,7 @@ export default {
   width: 80px;
   height: 30px;
   line-height: 30px;
+  -webkit-text-size-adjust: 100%;
   @include flex;
   overflow: hidden;
   .mini-picker-input {
@@ -71,7 +118,7 @@ export default {
     width: 100vw;
     height: 100vh;
     background: rgba(0, 0, 0, 0.3);
-    z-index:1991 - 1;
+    z-index: 1991 - 1;
   }
   .mini-picker-con {
     @include flex;
@@ -82,7 +129,7 @@ export default {
     height: 300px;
     background: #fff;
     overflow: hidden;
-    z-index:1991;
+    z-index: 1991;
     .mini-picker-btn {
       @include flex(row);
       padding: 20px 30px;
@@ -97,10 +144,12 @@ export default {
     .mini-picker-item {
       text-align: center;
       color: #808080;
-      overflow-x: hidden;
-      overflow-y: auto;
+      overflow: hidden;
       li {
-        padding: 20px;
+        @include flex;
+        justify-content: center;
+        box-sizing: border-box;
+        height:76px;
       }
     }
   }
